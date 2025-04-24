d132511fb7d38dd22d4bf120b5afb428a3b9de7f
asr-pdeu <24bch078@spt.pdpu.ac.in>
2025-04-24 23:55:25 +0530

    Update exceptionhandling.py

--- 24bch078/exceptionhandling.py
+++ 24bch078/exceptionhandling.py
@@ -1,1 +1,29 @@
+#11.1
 
+def get_integer_input():
+
+    while True:
+
+        user_input = input("Please enter an integer: ")
+
+        try:
+
+            user_int = int(user_input)
+
+            print(f"You entered the integer: {user_int}")
+
+            return user_int
+
+        except ValueError:
+
+            print("Error: That was not a valid integer. Please try again.")
+
+ 
+
+ 
+
+get_integer_input()
+
+ 
+
+ 
